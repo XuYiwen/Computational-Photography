117a081ef0cf854c9aed6f9e918ddb66f8692e70
Yiwen-Surface <xuywformal@gmail.com>
2015-09-28 23:12:05 -0500

    texture transfer completed.

--- PJ2 Image Quilting/mix_cut.m
+++ PJ2 Image Quilting/mix_cut.m
@@ -7,7 +7,7 @@ function mix = mix_cut(temp,addin,cut_mask)
         mix(:,:,i) = old(:,:,i)+new(:,:,i);
     end
     % test
-%     figure(1),imshow(~cut_mask);
+%     figure(1),imshow(cut_mask);
 %     figure(2),imshow(old);
 %     figure(3),imshow(new);
 %     figure(4),imshow(mix);
